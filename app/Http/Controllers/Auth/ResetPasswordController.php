0f5d4a6ead70546fba9a76a6e522e77d7be1a2c7
姜广庆 <hx@aumc.cc>
2019-11-13 15:40:29 +0800

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -17,13 +18,24 @@ class ResetPasswordController extends Controller
     | explore this trait and override any methods you wish to tweak.
     |
     */
-
+    
     use ResetsPasswords;
-
+    
     /**
      * Where to redirect users after resetting their password.
      *
      * @var string
      */
     protected $redirectTo = '/';
+    
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    
+    public function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密码更新成功,您已成功登录');
+        return redirect($this->redirectPath());
+    }
 }
