c2faf0876cd2ac864b704d678f56cc4b6d335be1
姜广庆 <hx@aumc.cc>
2019-11-20 08:25:41 +0000

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,11 +15,11 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-		 \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
-		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
+        \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
+        \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
-
+    
     /**
      * Register any authentication / authorization services.
      *
@@ -30,6 +32,10 @@ class AuthServiceProvider extends ServiceProvider
         //修改策略自动发现的逻辑
         Gate::guessPolicyNamesUsing(function ($modelClass) {
             return 'App\Policies\\' . class_basename($modelClass) . 'Policy';
+        });
+        Horizon::auth(function ($request) {
+            //是否是站长
+            return Auth::user()->hasRole('Founder');
         });
     }
 }
