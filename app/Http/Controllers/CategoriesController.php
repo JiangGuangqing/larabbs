fc970a5db283435737dc84297b878820002fdb8c
姜广庆 <hx@aumc.cc>
2019-11-21 07:21:41 +0000

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use Illuminate\Http\Request;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category, Request $request, Topic $topic,User $user)
+    public function show(Category $category, Request $request, Topic $topic,User $user,Link $link)
     {
         
         //读取分类 ID 关联的话题，并按每 20 条分页
@@ -18,7 +19,10 @@ class CategoriesController extends Controller
             ->where('category_id', $category->id)
             ->with('user', 'category')
             ->paginate(20);
+        
         $active_users = $user->getActiveUsers();
-        return view('topics.index', compact('topics', 'category','active_users'));
+        $links = $link->getAllCached();
+        
+        return view('topics.index', compact('topics', 'category','active_users','links'));
     }
 }
