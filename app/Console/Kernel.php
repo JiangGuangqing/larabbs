0744e7aea9da996ae8510e110fdd1ce0a293f747
姜广庆 <hx@aumc.cc>
2019-11-21 03:13:51 +0000

    活跃用户

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,19 +15,20 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //
     ];
-
+    
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('larabbs:calulate-active-user')->hourly();
     }
-
+    
     /**
      * Register the commands for the application.
      *
@@ -35,8 +36,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
+        $this->load(__DIR__ . '/Commands');
+        
         require base_path('routes/console.php');
     }
 }
