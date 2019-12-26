6272c5cbf00350f163a6a6ca013165b726a4c616
姜广庆 <hx@aumc.cc>
2019-12-26 07:27:53 +0000

    微信登录

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->middleware('throttle:' .
 
     //用户注册
     Route::post('users', 'UsersController@store')->name('users.store');
+
+    //第三方登录
+    Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')->where('social_type', 'weixin')->name('socials.authorizations.store');
 });
 
