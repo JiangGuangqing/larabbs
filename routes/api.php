5dc5f477bd3adfaf51e03556692c116d8d64b1bd
姜广庆 <hx@aumc.cc>
2020-01-07 08:11:04 +0000

    消息通知统计

--- routes/api.php
+++ routes/api.php
@@ -73,6 +73,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->middleware('throttle:' .
                 Route::delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')->name('topics.replies.destroy');
                 //通知列表
                 Route::get('notifications', 'NotificationsController@index')->name('notifications.index');
+                //通知统计
+                Route::get('notifications/stats', 'NotificationsController@stats')->name('notifications.stats');
             });
         });
 
